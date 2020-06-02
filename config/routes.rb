52a65f1511c5d7647c914dbdb3063099c695b9c2
Akrivi <angeli.vivian@gmail.com>
2020-06-02 14:15:24 +0200

    controllers for users/donations/reservvations

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :donations do
+  	resources :reservations, only: [ :create, :edit, :update, :destroy ]
+  	#resources :categories, only: [ :index, :show ]
+  end
 end
