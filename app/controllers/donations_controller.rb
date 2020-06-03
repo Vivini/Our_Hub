94232bd72305424c1baedadda8aa202159658a33
LorenzTroll <tetrix.t@googlemail.com>
2020-06-03 17:00:10 +0200

    created pundit policy for donations.

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,2 +1,35 @@
 class DonationsController < ApplicationController
+  before_action :set_donation, only: [:show, :edit, :update, :destroy]
+  def index
+    @donations = policy_scope(Donation)
+  end
+
+  def show
+    authorize @donation
+  end
+
+  def new
+    @donation = current_user.donations.new
+    authorize @donation
+  end
+
+  def edit
+  end
+
+  def create
+    @donation = current_user.donations.new(donation_params)
+    authorize @donation
+    if @donation.save
+      redirect_to @donation
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_donation
+    @donation = Donation.find(params[:id])
+    authorize @donation
+  end
 end
