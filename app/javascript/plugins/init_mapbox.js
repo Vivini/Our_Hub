e474496e02dd3c1cdb733827cc3f1e89e9716395
Akrivi <angeli.vivian@gmail.com>
2020-06-09 12:14:44 +0200

    map js

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,13 +2,18 @@ import mapboxgl from 'mapbox-gl';
 
 const mapElement = document.getElementById('map');
 
-const buildMap = () => {
-  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-  return new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/vivini/ckb686a1t3g6f1ionykd6v6gz'
-  });
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/vivini/ckb686a1t3g6f1ionykd6v6gz'
+    });
+  }
 };
+
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
@@ -27,14 +32,7 @@ const fitMapToMarkers = (map, markers) => {
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
 };
 
-const initMapbox = () => {
-  if (mapElement) {
-    const map = buildMap();
-    const markers = JSON.parse(mapElement.dataset.markers);
-    addMarkersToMap(map, markers);
-    fitMapToMarkers(map, markers);
-  }
-};
+
 
 
 export { initMapbox };
