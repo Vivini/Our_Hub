db9d1c9ac30b366b297b5285ee70c4f2a5f84f01
Holly McMahon <hmcmahon25@gmail.com>
2020-06-10 22:26:57 +0200

    meta tag set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,10 @@ class ApplicationController < ActionController::Base
     @white_navbar = true
   end
 
+  def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   private
 
   def skip_pundit?
