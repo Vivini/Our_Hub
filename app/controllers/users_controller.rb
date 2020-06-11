07b2b838ff51502ce923e8d0961296315e61cd81
LorenzTroll <tetrix.t@googlemail.com>
2020-06-11 23:52:23 +0200

    Created user dashboard with edit and delete user donation actions. Inplemented a div to display active reservations.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_white_navbar, only: [:show]
+
   def index
   @users = User.geocoded # returns users (organizations) with coordinates
   @markers = @users.map do |user|
